7fe8221cf4cc6ae4ce15ad71b9aca77123e4d0db
SolomonAnn <thss15_anyz@163.com>
2020-11-18 14:14:26 +0800

    resolve conflicts

--- server/src/main/java/org/apache/iotdb/db/engine/storagegroup/StorageGroupProcessor.java
+++ server/src/main/java/org/apache/iotdb/db/engine/storagegroup/StorageGroupProcessor.java
@@ -499,6 +499,7 @@ public class StorageGroupProcessor {
         Collections.addAll(upgradeFiles,
             fsFactory.listFilesBySuffix(upgradeFolder.getAbsolutePath(), TSFILE_SUFFIX));
       }
+      // if already move old files to upgradeFolder
       else if (upgradeFolder.exists()) {
         Collections.addAll(upgradeFiles,
             fsFactory.listFilesBySuffix(upgradeFolder.getAbsolutePath(), TSFILE_SUFFIX));
@@ -922,7 +923,8 @@ public class StorageGroupProcessor {
   public void asyncFlushMemTableInTsFileProcessor(TsFileProcessor tsFileProcessor) {
     writeLock();
     try {
-      if (!closingSequenceTsFileProcessor.contains(tsFileProcessor) && !closingUnSequenceTsFileProcessor.contains(tsFileProcessor)) {
+      if (!closingSequenceTsFileProcessor.contains(tsFileProcessor) &&
+          !closingUnSequenceTsFileProcessor.contains(tsFileProcessor)) {
         fileFlushPolicy.apply(this, tsFileProcessor, tsFileProcessor.isSequence());
       }
     } finally {
@@ -1097,7 +1099,8 @@ public class StorageGroupProcessor {
   public void asyncCloseOneTsFileProcessor(boolean sequence, TsFileProcessor tsFileProcessor) {
     //for sequence tsfile, we update the endTimeMap only when the file is prepared to be closed.
     //for unsequence tsfile, we have maintained the endTimeMap when an insertion comes.
-    if (closingSequenceTsFileProcessor.contains(tsFileProcessor) || closingUnSequenceTsFileProcessor.contains(tsFileProcessor)) {
+    if (closingSequenceTsFileProcessor.contains(tsFileProcessor) ||
+        closingUnSequenceTsFileProcessor.contains(tsFileProcessor)) {
       return;
     }
     logger.info("Async close tsfile: {}",
